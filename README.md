b77761cb99222a10ace6568034097b9e0c0cf27b
jdpublic <neil@jugglingduck.com>
2018-05-27 19:45:24 +0100

    initial base solution

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Triangles
 A little fun
+
+#Developer notes
+
+Projects are targeting .net framework 4.6.2 ([Download for dev pack](https://www.microsoft.com/net/download/thank-you/net462-developer-pack))
+
+Test projects using FluentAssertions ([gitbub site](https://github.com/fluentassertions/fluentassertions))
+
+
+
---
+++ test/WebApi.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WebApi.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebApi.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("85cf4479-36c4-457f-af9f-d6053e19da0b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
