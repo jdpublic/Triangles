bd477c4ef42768201262170c6bf56db49d8f9e20
jdpublic <neil@jugglingduck.com>
2018-05-28 22:52:24 +0100

    Added webapi and simple html test page

---
+++ src/WebApi/Controllers/ImageGridController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class ImageGridController : ApiController
+    {
+
+        // GET: api/ImageGrid/GetTriangle?row=a&col=1
+        // GET: api/ImageGrid/?row=a&col=1
+        [HttpGet]
+        public Triangle GetTriangle(string row, string col)
+        {
+            var t =  new Triangle() { RowName = row, ColumnName = col };
+            return t;
+        }
+
+        // POST: api/ImageGrid
+        public void Post([FromBody]string value)
+        {
+        }
+
+    }
+}
